513b53fc7d2f2588a8f55f54eba9a731b7cd0bc6
Vladimir Alemasov <avlal.nsu@gmail.com>
2021-12-20 01:53:02 +0700

    implemented drag n drop cards

---
+++ Assets/Scripts/Game/CardDragDrop.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Game
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class CardDragDrop : MonoBehaviour,
+        IBeginDragHandler, IEndDragHandler, IDragHandler
+    {
+        private RectTransform _cardTrf;
+        private CanvasGroup _canvasGroup;
+        private Canvas _canvas;
+
+        private Vector2 _startingAnchoredPosition;
+        private bool _wasDropped = false;
+
+        private void Awake()
+        {
+            _cardTrf = GetComponent<RectTransform>();
+            _canvasGroup = GetComponent<CanvasGroup>();
+
+            if (_canvas == null)
+            {
+                _canvas = GetComponentInParent<Canvas>();
+                if (_canvas == null)
+                {
+                    Debug.LogWarning($"{nameof(_canvas)} is not set!");
+                    enabled = false;
+                }
+            }
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            _startingAnchoredPosition = _cardTrf.anchoredPosition;
+            _canvasGroup.blocksRaycasts = false;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            _cardTrf.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _canvasGroup.blocksRaycasts = true;
+            if (!_wasDropped)
+            {
+                _cardTrf.anchoredPosition = _startingAnchoredPosition;
+            }
+        }
+
+        public void Drop()
+        {
+            _wasDropped = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/CardDropZone.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Game
+{
+    public class CardDropZone : MonoBehaviour, IDropHandler
+    {
+        public void OnDrop(PointerEventData eventData)
+        {
+            if (eventData.pointerDrag == null)
+            {
+                return;
+            }
+
+            var cardGo = eventData.pointerDrag;
+            var cardDrag = cardGo.GetComponent<CardDragDrop>();
+            if (cardDrag != null)
+            {
+                cardDrag.enabled = false;
+            }
+
+            var cardRectTrf = cardGo.GetComponent<RectTransform>();
+            if (cardRectTrf == null)
+            {
+                return;
+            }
+
+            var rectTransform = GetComponent<RectTransform>();
+            if (rectTransform == null)
+            {
+                return;
+            }
+
+            cardRectTrf.SetParent(rectTransform);
+            cardRectTrf.rotation = Quaternion.Euler(Vector3.zero);
+        }
+    }
+}
